6ad1780afaa6155cc11d4552b8930136ba11db51
michaelsantana95 <m95douglas@gmail.com>
2024-05-28 19:33:31 -0300

    Iteração do conteúdo de cada e todas as temporadas

--- src/main/java/br/com/alura/screematch/ScreematchApplication.java
+++ src/main/java/br/com/alura/screematch/ScreematchApplication.java
@@ -1,7 +1,6 @@
 package br.com.alura.screematch;
 
-import br.com.alura.screematch.model.DadosSerie;
-import br.com.alura.screematch.service.ConverteDados;
+import br.com.alura.screematch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,14 +14,8 @@ public class ScreematchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new br.com.alura.screenmatch.service.ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=2017f9f3");
-//		System.out.println(json);
-//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 
 	}
 }
